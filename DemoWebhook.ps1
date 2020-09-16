85e148ef453aa70b187864a651dcc7f59e70cec2
Trainer Microsoft <theok200915@outlook.com>
2020-09-16 10:49:55 +0200

    DemoWebhook

---
+++ DemoWebhook.ps1
@@ -0,0 +1,48 @@
+param
+(
+    [Parameter (Mandatory = $false)]
+    [object] $WebhookData
+)
+
+# If runbook was called from Webhook, WebhookData will not be null.
+if ($WebhookData) {
+
+    # Check header for message to validate request
+    if ($WebhookData.RequestHeader.message -eq 'StartedbyContoso')
+    {
+        Write-Output "Header has required information"}
+    else
+    {
+        Write-Output "Header missing required information";
+        exit;
+    }
+
+    # Retrieve VMs from Webhook request body
+    $vms = (ConvertFrom-Json -InputObject $WebhookData.RequestBody)
+
+    # Authenticate to Azure by using the service principal and certificate. Then, set the subscription.
+
+    Write-Output "Authenticating to Azure with service principal and certificate"
+    $ConnectionAssetName = "AzureRunAsConnection"
+    Write-Output "Get connection asset: $ConnectionAssetName"
+
+    $Conn = Get-AutomationConnection -Name $ConnectionAssetName
+            if ($Conn -eq $null)
+            {
+                throw "Could not retrieve connection asset: $ConnectionAssetName. Check that this asset exists in the Automation account."
+            }
+            Write-Output "Authenticating to Azure with service principal."
+            Add-AzAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint | Write-Output
+
+        # Start each virtual machine
+        foreach ($vm in $vms)
+        {
+            $vmName = $vm.Name
+            Write-Output "Starting $vmName"
+            Start-AzVM -Name $vm.Name -ResourceGroup $vm.ResourceGroup
+        }
+}
+else {
+    # Error
+    write-Error "This runbook is meant to be started from an Azure alert webhook only."
+}
