46d292ad453c9123577ac19ec57ed977149240f8
Trainer Microsoft <theok200915@outlook.com>
2020-09-16 12:08:21 +0200

    More exploring

--- Samples/TestWebhookdata.ps1
+++ Samples/TestWebhookdata.ps1
@@ -7,15 +7,23 @@ if($null -ne $WebhookData){
         $WebhookName    =   $WebhookData.WebhookName 
         $WebhookHeaders =   $WebhookData.RequestHeader  
         $WebhookBody    =   $WebhookData.RequestBody
-        
+        $WebhookMembers =   Get-Member -InputObject $WebhookData
+        $BodyObject     =   ConvertFrom-Json $WebhookData.RequestBody
 
-        Write-Output "------RAW WebhookData--------"
-        Write-Output $WebhookData
+        Write-Output "------Members--------"
+        Write-Output $WebhookMembers
         Write-Output "--------------"
 
         Write-Output $WebhookName
-        Write-Output $WebhookBody
         Write-Output $WebhookHeaders
+        Write-Output $WebhookBody
+
+        Write-Output "#############Object#############"
+        Write-Output $BodyObject
+        Write-Output "################################"
+
+
+
 }
 else {
     Write-Error "Runbook mean to be started only from webhook"
